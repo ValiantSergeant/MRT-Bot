a8d673bc55f652ecc23baa9df07c92d9a1f67cb5
Martin-Dépanne <martindepanne@users.noreply.github.com>
2026-02-10 20:55:55 +0100

    Update Events.js

--- Handler/Events.js
+++ Handler/Events.js
@@ -1,34 +1,38 @@
 import config from "../config.json" with { type: 'json' }
 import fs from "fs"
+import path from "path"
 
 export default async (bot) => {
-	const eventFiles = fs.readdirSync('./Events/').filter((file) => file.endsWith('.js'));
+    const eventFiles = fs.readdirSync('./Events/').filter((file) => file.endsWith('.js'));
 
-	for (const file of eventFiles) {
-		const event = (await import(`../Events/${file}`)).default;
+    for (const file of eventFiles) {
+        const event = (await import(`../Events/${file}`)).default;
 
-		if (event.once) {
-			bot.once(event.name, (...args) => event.execute(...args, bot, config));
-		} else {
-			bot.on(event.name, (...args) => event.execute(...args, bot, config));
-		}
-		console.log(`[EVENT] > ${file}`);
-	}
+        if (event.once) {
+            bot.once(event.name, (...args) => event.execute(...args, bot, config));
+        } else {
+            bot.on(event.name, (...args) => event.execute(...args, bot, config));
+        }
+        console.log(`[EVENT] > ${file}`);
+    }
 
-	const eventSubFolders = fs.readdirSync('./Events/').filter((folder) => !folder.endsWith('.js'));
+    const eventSubFolders = fs.readdirSync('./Events/').filter((folder) => {
+        const fullPath = path.join('./Events/', folder);
+        return fs.statSync(fullPath).isDirectory();
+    });
 
-	for (const folder of eventSubFolders) {
-		const subEventFiles = fs.readdirSync(`./Events/${folder}/`).filter((file) => file.endsWith('.js'));
+    for (const folder of eventSubFolders) {
+        const subEventFiles = fs.readdirSync(`./Events/${folder}/`).filter((file) => file.endsWith('.js'));
 
-		for (const file of subEventFiles) {
-			const event = (await import(`../Events/${folder}/${file}`)).default;
+        for (const file of subEventFiles) {
+            const event = (await import(`../Events/${folder}/${file}`)).default;
 
-			if (event.once) {
-				bot.once(event.name, (...args) => event.execute(...args, bot, config));
-			} else {
-				bot.on(event.name, (...args) => event.execute(...args, bot, config));
-			}
-			console.log(`[EVENT] > ${file} - ${folder}`);
-		}
-	}
+            if (event.once) {
+                bot.once(event.name, (...args) => event.execute(...args, bot, config));
+            } else {
+                bot.on(event.name, (...args) => event.execute(...args, bot, config));
+            }
+            console.log(`[EVENT] > ${file} - ${folder}`);
+        }
+    }
 };
